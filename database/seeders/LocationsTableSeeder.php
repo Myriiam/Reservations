2622c257eecf15e9f613b090bb6c03005154e360
Grégory Van Ossel <vanosselgregory@gmail.com>
2021-04-12 05:02:47 +0200

    location page design + Leaflet Map

--- database/seeders/LocationsTableSeeder.php
+++ database/seeders/LocationsTableSeeder.php
@@ -33,6 +33,8 @@ class LocationsTableSeeder extends Seeder
                 'locality_id' =>null,
                 'website'=>'https://www.lavenerie.be',
                 'phone'=>'+32 (0)2/663.85.50',
+                'latitude' => 54.720,
+                'longitude' => 4.29,
             ],
             [
                 'slug'=>null,
@@ -42,6 +44,8 @@ class LocationsTableSeeder extends Seeder
                 'locality_id' =>null,
                 'website'=>null,
                 'phone'=>null,
+                'latitude' => 58.720,
+                'longitude' => 5.39
             ],
             [
                 'slug'=>null,
@@ -51,6 +55,8 @@ class LocationsTableSeeder extends Seeder
                 'locality_id' =>null,
                 'website'=>'http://www.lasamaritaine.be/',
                 'phone'=>null,
+                'latitude' => 50.720,
+                'longitude' => 4.39
             ],
             [
                 'slug'=>null,
@@ -60,6 +66,8 @@ class LocationsTableSeeder extends Seeder
                 'locality_id' =>null,
                 'website'=>'http://www.espacemagh.be',
                 'phone'=>'+32 (0)2/274.05.10',
+                'latitude' => 52.720,
+                'longitude' => 4.99
             ],
         ];
 
@@ -74,6 +82,8 @@ class LocationsTableSeeder extends Seeder
                 'locality_id' => $locality->id,	//Référence à la table localities
                 'website' => $data['website'],
                 'phone' => $data['phone'],
+                'latitude' => $data['latitude'],
+                'longitude' => $data['longitude'],
             ]);
         }
     }
---
+++ public/js/leaflet.js
@@ -0,0 +1,11 @@
+console.log("script JS");
+
+   
+    
+
+
+
+
+ 
+ 
+ 
