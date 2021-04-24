6829f82883c0cf5847e58829bd315efdfbd6711c
Myriam K <kadiimyriam@hotmail.com>
2021-04-24 12:48:06 +0200

    roles in profil page updated

--- resources/views/profil/show.blade.php
+++ resources/views/profil/show.blade.php
@@ -1,9 +1,8 @@
 <x-app-layout>
-    <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __($resource) }}  
+    <x-slot name="header"></x-slot>
+    <h2 class="font-semibold text-5xl text-center text-red-600 leading-tight">
+            {{ __('Profile Page') }}  
     </h2>
-    </x-slot>
     <main class= "content">
         <div class= 'flex items-center justify-center'>
             <div class= 'bg-white w-2/3 mt-10 mb-10 rounded-lg border-2 border-red-600'>
@@ -15,14 +14,27 @@
                         <p> Mon login : {{ $user->login }}</p>
                         <p> Mon email : {{ $user->email }}</p>
                         <p> Langue : {{ $user->langue }}</p>
-                        <p> Mon rôle : {{ $role }}</p>
-                        <p class="mt-5 mb-2 text-red-600 underline"> Mes spectacles : </p>
+                        @if (count($roleUser) > 1) 
+                            <p class="mt-5 mb-2 text-red-600 underline"> Mes rôles :</p>
+                        @else
+                            <p class="mt-5 mb-2 text-red-600 underline"> Mon rôle : </p>;
+                        @endif
                         <ul class="list-disc list-inside">
-                        <!-- ajouter le code if(le nombre de spectacle est => 1 alors on affiche les spectacles sinon on écrit aucune repres)-->
-                            @foreach ($user->representations as $allShows)
-                                <li>{{ $allShows->show->title }}</li>  
+                            @foreach ($roleUser as $roles)
+                              <li>{{ $roles->role }}</li>  
                             @endforeach
                         </ul>
+                        <p class="mt-5 mb-2 text-red-600 underline"> Mes spectacles : </p>
+                        @if (count($user->representations) == 0) 
+                        <p>Acune représentation pour le moment</p>
+                        @else
+                            <ul class="list-disc list-inside">
+                            <!-- ajouter le code if(le nombre de spectacle est => 1 alors on affiche les spectacles sinon on écrit aucune repres)-->
+                                @foreach ($user->representations as $allShows)
+                                    <li>{{ $allShows->show->title }}</li>  
+                                @endforeach
+                            </ul> 
+                        @endif
                     </div>
                 </div>
             </div>
