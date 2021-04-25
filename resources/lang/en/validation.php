a663d0909d4b9ea10b76aa645b1c1406dffb6c92
Simon Oldenhove <simonoldenhove@gmail.com>
2021-04-25 11:16:01 +0200

    translations

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -12,7 +12,7 @@ return [
     | as the size rules. Feel free to tweak each of these messages here.
     |
     */
-
+    'error' => 'Oops, something went wrong.',
     'accepted' => 'The :attribute must be accepted.',
     'active_url' => 'The :attribute is not a valid URL.',
     'after' => 'The :attribute must be a date after :date.',
---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Identifiants incorrects.',
+    'password' => 'Le mot de passe fournit est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+
+];
