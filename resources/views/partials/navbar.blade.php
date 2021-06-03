1336c32927538e8af14f74c1bff7d88da2cdb2d6
Grégory Van Ossel <63047472+GregVo23@users.noreply.github.com>
2021-06-03 14:59:36 +0200

    add stripe key on env example +

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -102,7 +102,7 @@
       <div class="pt-5 pb-6 px-5">
         <div class="flex items-center justify-between">
           <div>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-red-800.svg" alt="Workflow">
+            <!--  IMG   -->
           </div>
           <div class="-mr-2">
             <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
