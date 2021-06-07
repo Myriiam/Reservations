0658cfbe2792b9581d498fd4a02a13898eb15cdb
Grégory Van Ossel <vanosselgregory@gmail.com>
2021-06-08 00:51:24 +0200

    add link to style.css on pages: welcome, a propos

--- resources/views/show/confirmation.blade.php
+++ resources/views/show/confirmation.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="px-12 font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="px-12 text-transparentfont-semibold text-xl text-gray-800 leading-tight">
             {{ __('Confirmation') }}
         </h2>
     </x-slot>
