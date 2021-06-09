4a9c94797743bd5a6a64737ce0e8723b8bf082b0
Grégory Van Ossel <vanosselgregory@gmail.com>
2021-06-09 17:23:24 +0200

    carbon parse date on NewsItem Model

--- app/Models/NewsItem.php
+++ app/Models/NewsItem.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Spatie\Feed\Feedable;
 use Spatie\Feed\FeedItem;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class NewsItem extends Show implements Feedable
 {
@@ -14,7 +15,7 @@ class NewsItem extends Show implements Feedable
             ->id($this->id)
             ->title($this->title)
             ->summary(htmlEntities(substr($this->description, 0, 60)))
-            ->updated($this->updated_at ?? $this->created_at)
+            ->updated(Carbon::parse($this->updated_at ?? $this->created_at))
             ->link(env('APP_URL')."/reservations/show/{$this->id}")
             ->authorName(env('APP_CONTACT'));
     }
